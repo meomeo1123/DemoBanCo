d8813e9a3ff080948d6256da1276a7bc3a13abce
meomeo1123 <nntua7nd2020@gmail.com>
2023-10-25 00:43:50 +0700

    DemoCoTuong

---
+++ DemoBanCo_20DTHE2/Program.cs
@@ -0,0 +1,97 @@
+﻿using DemoBanCo_20DTHE2.Hubs;
+using DemoBanCo_20DTHE2.Models;
+using Libs;
+using Libs.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+}, ServiceLifetime.Transient);
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+var tokenValidationParameter = new TokenValidationParameters()
+{
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(key),
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidAudience = builder.Configuration["JwtConfig:ValidAudience"],
+    ValidIssuer = builder.Configuration["JwtConfig:ValidIssuer"],
+    RequireExpirationTime = false,
+    ValidateLifetime = true,
+    ClockSkew = TimeSpan.Zero
+};
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = tokenValidationParameter;
+});
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedEmail = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("BossOnly", policy => policy.RequireRole("Boss"));
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+    options.AddPolicy("AllRole", policy => policy.RequireAssertion(context =>
+                                            context.User.IsInRole("Boss")
+                                            || context.User.IsInRole("Admin")
+                                            || context.User.IsInRole("User")));
+    options.AddPolicy("All", policy => policy.RequireAssertion(context =>
+                                            context.User.IsInRole("Boss")
+                                            && context.User.IsInRole("Admin")
+                                            && context.User.IsInRole("User")));
+    options.AddPolicy("AdminPolicyInsert", policy => policy.RequireRole("Admin").RequireClaim("Username", "admin"));
+});
+builder.Services.AddSingleton(tokenValidationParameter);
+
+builder.Services.AddTransient<ChessService>();
+builder.Services.AddTransient<TokenService>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Login}/{id?}");
+app.MapHub<ChatHub>("/chatHub");
+app.Run();
